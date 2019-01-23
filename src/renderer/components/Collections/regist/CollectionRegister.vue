3624e5c794469b68294e5371c0c823ca9510f2f0
kstory8715 <kstory8715@gmail.com>
2019-01-23 23:19:16 +0900

    .

--- src/renderer/components/Collections/regist/CollectionRegister.vue
+++ src/renderer/components/Collections/regist/CollectionRegister.vue
@@ -35,8 +35,8 @@ export default {
       type: Boolean,
       default: false
     },
-    data: Object,
-    playType: String
+    data: null,
+    playType: null
   },
   data() {
     return {
@@ -61,85 +61,69 @@ export default {
             const options = {
               selector: {
                 type: {
-                  $eq: this.playType === 'play' ? 
-                       "myplaylist" : 
-                       "mychannel"
+                  $eq: this.playType === "play" ? "myplaylist" : "mychannel"
                 },
                 userId: {
                   $eq: user
                 },
                 playlistId: {
-                  $eq: this.playType === 'play' ? 
-                       this.data.playlistId.split(":")[1] : 
-                       this.data.channelPlaylistId
+                  $eq:
+                    this.playType === "play"
+                      ? this.data.playlistId.split(":")[1]
+                      : this.data.channelPlaylistId
                 }
               }
-            }
-            this.createIndex(["type", "userId", "playlistId"])
-              .then(() => {
-                return this.$test
-                  .find(options).then(result => {
-                    const doc = result.docs[0]
-                    if (doc) {
-                      this.$test.remove(doc).then(res => {
-                        if (res.ok) {
-                          this.isToggle = false;
-                          this.$emit("toggle", this.isToggle);
-                          this.$emit("callback", true);
-                        }
-                      })
+            };
+            this.createIndex(["type", "userId", "playlistId"]).then(() => {
+              return this.$test.find(options).then(result => {
+                const doc = result.docs[0];
+                if (doc) {
+                  this.$test.remove(doc).then(res => {
+                    if (res.ok) {
+                      this.isToggle = false;
+                      this.$emit("toggle", this.isToggle);
+                      this.$emit("callback", true);
                     }
-                  })
+                  });
+                }
               });
-
+            });
           }
         } else {
           if (this.data) {
-            let listType = "";
-            if (this.playType === 'play') {
-              listType = "myplaylist"
-            } else {
-              listType = "mychannel"
-            }
-
-            // 나의 아이디로 등록 된 콜렉션 리스트 조회
+            const options = {
+              selector: {
+                type: {
+                  $eq: this.playType === "play" ? "myplaylist" : "mychannel"
+                },
+                userId: {
+                  $eq: user
+                }
+              }
+            };
             this.createIndex(["type", "userId"]).then(() => {
-              return this.$test
-                .find({
-                  selector: {
-                    type: {
-                      $eq: listType
-                    },
-                    userId: {
-                      $eq: this.getUserId()
-                    }
-                  }
-                })
-                .then(result => {
-                  const docs = result.docs;
-                  if (docs.length > 0) {
-                    let findToItem = "";
-                    if (this.playType === 'play') {
-                      findToItem = this.$lodash.find(docs, {
-                        playlistId: this.data.playlistId
-                      });
-                    } else {
-                      findToItem = this.$lodash.find(docs, {
-                        channelId: this.data.list[0].channelId
-                      });
-                    }
-                    if (findToItem) {
-                      // 이미 등록 된 컬렉션
-                      this.alreadyDialog();
-                    } else {
-                      // 등록되지 않은 콜렉션이면 등록한다.
-                      this.addCollection(this.data);
-                    }
+              return this.$test.find(options).then(result => {
+                const docs = result.docs;
+                if (docs.length > 0) {
+                  let findToItem = "";
+                  if (this.playType === "play") {
+                    findToItem = this.$lodash.find(docs, {
+                      playlistId: this.data.playlistId
+                    });
                   } else {
-                    // 등록되지 않은 콜렉션이면 등록한다.
-                    this.addCollection(this.data);
+                    findToItem = this.$lodash.find(docs, {
+                      channelId: this.data.list[0].channelId
+                    });
                   }
-                });
+                  if (findToItem) {
+                    this.alreadyDialog(); // 이미 등록 된 컬렉션
+                  } else {
+                    this.addCollection(this.data); // 등록되지 않은 콜렉션이면 등록한다.
+                  }
+                } else {
+                  this.addCollection(this.data);
+                }
+              });
             });
           }
         }
@@ -160,6 +144,7 @@ export default {
         created: this.$moment().format("YYYY-MM-DD HH:mm:ss")
       };
       if (this.playType === "play") {
+        // PLAY LIST
         data.type = "myplaylist";
         data.thumbnails = item.list[0].imageInfo;
         this.$test.post(data).then(result => {
@@ -169,7 +154,7 @@ export default {
           }
         });
       } else {
-        // 채널
+        // CHANNEL
         let requestURL = $commons.youtubeChannelSearch(data.channelId);
         this.$http.get(requestURL).then(res => {
           data.thumbnails = res.data.items[0].snippet.thumbnails.medium.url;
