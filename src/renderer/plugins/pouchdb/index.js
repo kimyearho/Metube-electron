5982ea9d1af825d8311e1fdb381cce9fe9da0907
kstory8715 <kstory8715@gmail.com>
2019-02-03 01:51:55 +0900

    .

--- src/renderer/plugins/pouchdb/index.js
+++ src/renderer/plugins/pouchdb/index.js
@@ -10,16 +10,11 @@ let PouchDB = require("pouchdb-core")
 
 // 개발용
 // pouchdb-server or couchdb
-// Vue.prototype.$local = new PouchDB("http://localhost:5984/metube")
+// Vue.prototype.$test = new PouchDB("http://localhost:5984/sample")
 
 // 로컬
-// Vue.prototype.$local = new PouchDB("metubev3")
+Vue.prototype.$test = new PouchDB("metubev145")
 
 // 서비스
 // App.vue -> onNewReleaseCheck()
 Vue.prototype.$db = new PouchDB("http://202.182.100.137/metube")
-
-// 샘플
-// Vue.prototype.$test = new PouchDB("http://localhost:5984/sample")
-// Vue.prototype.$test = new PouchDB("http://202.182.100.137/sample")
-Vue.prototype.$test = new PouchDB("metubev145")
