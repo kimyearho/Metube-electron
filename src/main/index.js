7fd5f0f28cdf55d4ae8099208a90f11981feca5e
ken <kstory8715@gmail.com>
2019-03-26 15:18:41 +0900

    배포 준비

--- src/main/index.js
+++ src/main/index.js
@@ -1,29 +1,29 @@
-import { app, BrowserWindow, ipcMain, dialog, shell } from "electron";
-import { googleLogin, getOauth2Client } from "../auth/auth";
-import { exec } from "child_process";
-import log from "electron-log";
-import request from "request";
-import { autoUpdater } from "electron-updater";
+import { app, BrowserWindow, ipcMain, dialog, shell } from "electron"
+import { googleLogin, getOauth2Client } from "../auth/auth"
+import { exec } from "child_process"
+import log from "electron-log"
+import request from "request"
+import { autoUpdater } from "electron-updater"
 
-let player;
-let mainWindow;
-let willQuitApp = false;
+let player
+let mainWindow
+let willQuitApp = false
 const winURL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:9080`
-    : `file://${__dirname}/index.html`;
+    : `file://${__dirname}/index.html`
 
-const gotTheLock = app.requestSingleInstanceLock();
+const gotTheLock = app.requestSingleInstanceLock()
 
 if (process.platform === "darwin") {
-  app.dock.hide();
+  app.dock.hide()
 }
 
-app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
+app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required")
 
 // Create Main Window
 function createWindow() {
-  log.info("================ >>> APP START");
+  log.info("================ >>> APP START")
 
   mainWindow = new BrowserWindow({
     width: 368,
@@ -36,122 +36,120 @@ function createWindow() {
     webPreferences: {
       backgroundThrottling: false
     }
-  });
+  })
 
-  mainWindow.setMenu(null);
-  mainWindow.loadURL(winURL);
+  mainWindow.setMenu(null)
+  mainWindow.loadURL(winURL)
 
   if (process.env.NODE_ENV !== "production") {
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools()
   }
 
   mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 
   // Main Window on Ready
   mainWindow.webContents.on("did-finish-load", () => {
-    if (player) return;
+    if (player) return
     player = new BrowserWindow({
       width: 420,
       height: 280,
       title: "Player"
-    });
-    player.setMenu(null);
+    })
+    player.setMenu(null)
 
-    let playerPath;
+    let playerPath
 
     if (process.env.NODE_ENV !== "production") {
-      playerPath = "http://localhost:7070";
+      playerPath = "http://localhost:7070"
     } else {
-      playerPath = "http://sharepod.kr";
+      playerPath = "http://sharepod.kr"
     }
-    player.loadURL(playerPath);
+    player.loadURL(playerPath)
     player.on("close", e => {
       if (!willQuitApp) {
         dialog.showErrorBox(
           "Oops! 🤕",
           "Sorry, player window cannot be closed. You can only minimize it."
-        );
-        e.preventDefault();
+        )
+        e.preventDefault()
       }
-    });
-  });
+    })
+  })
 
   mainWindow.on("close", e => {
     if (willQuitApp) {
       // the user tried to quit the app
-      player = null;
-      mainWindow = null;
+      player = null
+      mainWindow = null
     } else {
       // the user only tried to close the win
       if (process.platform !== "darwin") {
-        exec("taskkill /f /im MeTube.exe");
+        exec("taskkill /f /im MeTube.exe")
       } else {
-        e.preventDefault();
-        mainWindow.hide();
+        e.preventDefault()
+        mainWindow.hide()
       }
     }
-  });
+  })
 
   mainWindow.on("restore", e => {
-    e.preventDefault();
-    mainWindow.show();
-  });
+    e.preventDefault()
+    mainWindow.show()
+  })
 }
 
-require("../analiytics/analytics")(app);
+require("../analiytics/analytics")(app)
 
 if (!gotTheLock) {
-  app.quit();
+  app.quit()
 } else {
   app.on("second-instance", (event, commandLine, workingDirectory) => {
     // Someone tried to run a second instance, we should focus our window.
     if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore();
-      mainWindow.focus();
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
     }
-  });
+  })
 
   // App start
   app.on("ready", () => {
     // local player server
     if (process.env.NODE_ENV !== "production") {
       if (process.platform !== "darwin") {
-        exec(".\\node_modules\\.bin\\http-server ./player -p 7070");
+        exec(".\\node_modules\\.bin\\http-server ./player -p 7070")
       }
     }
-    createWindow();
-  });
+    createWindow()
+  })
 }
 
 app.on("activate", () => {
-  mainWindow.show();
-});
+  mainWindow.show()
+})
 app.on("before-quit", () => {
-  willQuitApp = true;
-});
+  willQuitApp = true
+})
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 // Google Auth Youtube API Get AccessToken
 ipcMain.on("main:googleAuth", () => {
-  console.log("Google Youtube Oauth2.0 Start");
+  console.log("Google Youtube Oauth2.0 Start")
   googleLogin(mainWindow).then(code => {
-    console.log("code => ", code);
     getOauth2Client()
       .getToken(code)
       .then(res => {
-        console.log("res => ", res);
         if (res.tokens.access_token) {
           let requestURL = `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${
             res.tokens.access_token
-          }`;
-          console.log("requestUrl => ", requestURL);
+          }`
+          console.log("requestUrl => ", requestURL)
           request.get(requestURL, (error, response, body) => {
             console.log(response)
             if (response.statusCode === 200) {
@@ -159,127 +157,122 @@ ipcMain.on("main:googleAuth", () => {
                 resultCode: 200,
                 resultMsg: "success",
                 body: body
-              });
+              })
             }
-          });
+          })
         } else {
           mainWindow.webContents.send("render:googleAuth", {
             resultCode: -1,
             resultMsg: "error"
-          });
+          })
         }
-      });
-  });
-});
+      })
+  })
+})
 
 // Window Close
 ipcMain.on("button:close", () => {
   if (process.platform !== "darwin") {
-    exec("taskkill /f /im MeTube.exe");
-    app.quit();
+    exec("taskkill /f /im MeTube.exe")
+    app.quit()
   } else {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 ipcMain.on("eventLogger", (e, args) => {
   if (args.value) {
-    log.info(args.message, args.value);
+    log.info(args.message, args.value)
   } else {
-    log.info(args.message);
+    log.info(args.message)
   }
-});
+})
 
 // Window minimal
 ipcMain.on("button:minimize", () => {
-  mainWindow.minimize();
-});
+  mainWindow.minimize()
+})
 
 // Youtube Watch
 ipcMain.on("button:watchYoutubePopup", (e, url) => {
-  shell.openExternal(url);
-});
+  shell.openExternal(url)
+})
 
 // Setting - AlwaysStop
 ipcMain.on("option:alwaystop", (e, args) => {
   if (args) {
-    mainWindow.setAlwaysOnTop(true);
+    mainWindow.setAlwaysOnTop(true)
   } else {
-    mainWindow.setAlwaysOnTop(false);
+    mainWindow.setAlwaysOnTop(false)
   }
-});
+})
 
 // Setting - Player Hide / Show
 ipcMain.on("isPlayer", (e, args) => {
   if (args) {
-    player.hide();
+    player.hide()
   } else {
-    player.show();
+    player.show()
   }
-});
-
-// Setting - New Releases Download
-ipcMain.on("showGit", (e, args) => {
-  shell.openExternal("https://github.com/kimyearho/MeTube/releases");
-});
+})
 
 // Window to Player Events
 ipcMain.on("win2Player", (e, args) => {
   try {
     if (process.env.NODE_ENV === "development") {
-      console.log("main -> " + args);
+      console.log("main -> " + args)
     }
-    player.webContents.send("win2Player", args);
+    player.webContents.send("win2Player", args)
   } catch (err) {
     /* window already closed */
   }
-});
+})
 
 // Player to Window Events
 ipcMain.on("player2Win", (e, args) => {
   try {
     if (process.env.NODE_ENV === "development") {
-      console.log("sub -> " + args);
+      console.log("sub -> " + args)
     }
-    mainWindow.webContents.send("player2Win", args);
+    mainWindow.webContents.send("player2Win", args)
   } catch (err) {
     /* window already closed */
   }
-});
+})
 
 if (process.env.NODE_ENV === "production") {
-  // 프로덕션 상태라면 업데이트 요청을 앱 시작 시 요청함.
-  // autoUpdater.checkForUpdates();
+  // 프로덕션 상태라면 최신 업데이트 요청을 앱 시작 시 요청함.
+  autoUpdater.checkForUpdates()
 
   // 1시간 간격으로 최신 업데이트 조회
   setInterval(() => {
-    autoUpdater.checkForUpdates();
-  }, 3600000);
+    autoUpdater.checkForUpdates()
+  }, 3600000)
 
   // 업데이트할 내용이 없음
   autoUpdater.on("update-not-available", () => {
-    log.info(">>> No Updates, Current version is up-to-date.");
-  });
+    log.info(">>> No Updates, Current version is up-to-date.")
+  })
 
   // 업데이트가 있으므로 사용자에게 알림.
+  // 1.6.6 버전부터 강제 업데이트로 변경 됨.
   autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
     const dialogOpts = {
       type: "info",
       buttons: ["Restart"],
       title: "Application Update",
       message: process.platform === "win32" ? releaseNotes : releaseName,
-      detail:
-        "The update has been verified. Click the `Restart` button to update"
-    };
+      detail: "The update has been verified. Click the `Restart` button to update"
+    }
 
     dialog.showMessageBox(dialogOpts, response => {
-      if (response === 0) autoUpdater.quitAndInstall();
-    });
-  });
+      if (response === 0) autoUpdater.quitAndInstall()
+    })
+  })
 
   // 업데이트 오류
   autoUpdater.on("error", message => {
-    log.info(">>> 애플리케이션을 업데이트하는 도중 오류가 발생하였습니다.");
-    log.info(message);
-  });
+    log.info(">>> 애플리케이션을 업데이트하는 도중 오류가 발생하였습니다.")
+    log.info(message)
+  })
 }
