38ebc2ea3f03bd1ecee00dbd1b70ef815f559e8a
ken <kstory8715@gmail.com>
2019-03-26 14:50:37 +0900

    faq 수정

--- src/renderer/components/Commons/Faq/Faq.vue
+++ src/renderer/components/Commons/Faq/Faq.vue
@@ -33,25 +33,25 @@
         <p>I am a full stack web developer and I like to make something. I like YouTube and love to listen to a lot of music on YouTube. It was a bit uncomfortable for me to listen to music with YouTube turned on. YouTube is a good video platform, not a music player. So I was thinking about a music player that is not inconvenient, and I am developing to this day This project will continue until the day I dislike music.</p>
       </el-collapse-item>
       <el-collapse-item
-        title="Future plans"
+        title="Why can not I end the YouTube player?"
         name="2"
+      >
+        <p>The reason for using a separate YouTube player is that it is an open source policy for using the YouTube API. If you use an open API, it is your obligation to adhere to the prescribed policies.</p>
+      </el-collapse-item>
+      <el-collapse-item
+        title="Future plans"
+        name="3"
       >
         <p>I have been developing for about a year alone.
           Perhaps it is the longest running work I have done on my personal projects. We have, and will continue to, improve features and stability and performance that may be needed or more convenient to use. We are envisioning features that are still more convenient, and are expected to be developed. However, I am developing by myself and can not go fast because I work in parallel with my work life. We will continue to develop our apps as best we can.</p>
       </el-collapse-item>
       <el-collapse-item
         title="Feedback"
-        name="3"
+        name="4"
       >
         <p>i am developing and design and testing
           Therefore, the development speed is slow. If you're having trouble using the app, please post an issue to github or email us.
           Please contact me if you have a good idea or want to ask a question. (kstory8715@gmail.com)</p>
-      </el-collapse-item>
-      <el-collapse-item
-        title="Commercial use"
-        name="4"
-      >
-        <p>"Metube" is not used in any commercial sense. Update server and player server are running separately. All of them are used as expenses. I do not think that I am a big problem in using develop because I like it. But if you can give me a donation to buy me a cup of coffee, I am satisfied with it. </p>
       </el-collapse-item>
     </el-collapse>
   </div>
